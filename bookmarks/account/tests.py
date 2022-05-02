6d9e5239b00d72eaddf39b9344b8d92ae3bc4027
erebodino <erebodino@gmail.com>
2022-05-02 10:47:57 -0300

    black agregated and formatting

--- bookmarks/account/tests.py
+++ bookmarks/account/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
+
 def test_tru_for_CI():
     lala = 0
     assert True
+
 
 # Create your tests here.
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,20 +1,18 @@
-
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    #path('login/',views.user_login, name='login'),
-    #path('login/',auth_view.LoginView.as_view(),name='login'),
-    #path('logout/',auth_view.LogoutView.as_view(),name='logout'),
-    path('',views.dashboard, name='dashboard'),
+    # path('login/',views.user_login, name='login'),
+    # path('login/',auth_view.LoginView.as_view(),name='login'),
+    # path('logout/',auth_view.LogoutView.as_view(),name='logout'),
+    path("", views.dashboard, name="dashboard"),
     # path('password_change/',auth_view.PasswordChangeView.as_view(),name='password_change'),
     # path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(),name='password_change_done'),
     # path('password_reset/', auth_view.PasswordResetView.as_view(),name='password_reset'),
     # path('password_reset/done/',auth_view.PasswordResetDoneView.as_view(),name='password_reset_done'),
     # path('reset/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     # path('reset/done/',auth_view.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-    path('', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
-
+    path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
